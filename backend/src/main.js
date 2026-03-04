d1baf027882eb5acf75978f4cd2a0187f71c1405
Agustin Xocua Dimayuga <Axocuadi@calpoly.edu>
2026-03-03 20:57:45 -0800

    Removed Shared Test

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,14 +1,10 @@
 import express from "express";
 import { getEnvVar } from "./getEnvVar.js";
-import { SHARED_TEST } from "./shared/example.js";
 import { VALID_ROUTES } from "./shared/ValidRoutes.js";
 
 const PORT = Number.parseInt(getEnvVar("PORT", false), 10) || 3000;
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello, World " + SHARED_TEST);
-});
 app.get(Object.values(VALID_ROUTES), (req, res) => {
   res.sendFile("index.html", { root: STATIC_DIR });
 });
