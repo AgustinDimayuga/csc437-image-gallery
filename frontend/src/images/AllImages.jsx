f4b1d328f95e57f2df651b26772b95f4277e66c5
Agustin Xocua Dimayuga <Axocuadi@calpoly.edu>
2026-03-04 10:46:00 -0800

    added fetch

--- frontend/src/images/AllImages.jsx
+++ frontend/src/images/AllImages.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MainLayout } from "../MainLayout.jsx";
 import { fetchAll } from "./ImageFetcher.js";
 import { ImageGrid } from "./ImageGrid.jsx";
 
 export function AllImages() {
   const [imageData, _setImageData] = useState(fetchAll);
+  const [loadingState, setLoadingState] = useState(true);
+  const [errorDuringFetch, setErrorDuringFetch] = useState("");
+
+  useEffect(() => {
+    fetch("/api/images");
+  }, []);
+
   return (
     <>
       <h2>All Images</h2>
+      {loadingState && <span>Loading...</span>}
+      {errorDuringFetch.length !== 0 && <span>errorDuringFetch</span>}
       <ImageGrid images={imageData} />
     </>
   );
