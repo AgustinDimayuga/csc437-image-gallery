cb948cea0bbe237ca41fc744908e68d75c7dd9e6
Agustin Xocua Dimayuga <Axocuadi@calpoly.edu>
2026-03-03 21:00:00 -0800

    Added Better Paths

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { getEnvVar } from "./getEnvVar.js";
-import { VALID_ROUTES } from "./shared/ValidRoutes.js";
+import { VALID_ROUTES } from "../../frontend/src/shared/ValidRoutes.js";
+import { SHARED_TEST } from "../../frontend/src/shared/example.js";
 
 const PORT = Number.parseInt(getEnvVar("PORT", false), 10) || 3000;
 const app = express();
 
+app.get("/hello", (req, res) => {
+  res.send("Hello, World " + SHARED_TEST);
+});
 app.get(Object.values(VALID_ROUTES), (req, res) => {
   res.sendFile("index.html", { root: STATIC_DIR });
 });
