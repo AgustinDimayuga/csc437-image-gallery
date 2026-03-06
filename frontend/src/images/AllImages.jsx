cc9740f660a8377cfe87c59d8342281b91da01f3
AgustinDimayuga <axocuadi@calpoly.edu>
2026-03-05 19:55:08 -0800

    lab 20 done

--- frontend/src/images/AllImages.jsx
+++ frontend/src/images/AllImages.jsx
@@ -2,35 +2,10 @@ import { useEffect, useState } from "react";
 import { MainLayout } from "../MainLayout.jsx";
 import { fetchAll } from "./ImageFetcher.js";
 import { ImageGrid } from "./ImageGrid.jsx";
+import { useFetch } from "./useFetch.js";
 
 export function AllImages() {
-  const [imageData, _setImageData] = useState([]);
-  const [loadingState, setLoadingState] = useState(true);
-  const [errorDuringFetch, setErrorDuringFetch] = useState("");
-
-  useEffect(() => {
-    async function fetchImages() {
-      try {
-        const response = await fetch("/api/images");
-        // If response is an error (ie., not okay throw error)
-        if (!response.ok) {
-          throw new Error(
-            `Error: HTTP ${response.status} ${response.statusText}`,
-          );
-        }
-
-        const result = await response.json();
-        _setImageData(result);
-      } catch (error) {
-        console.error(error.message);
-        setErrorDuringFetch(error.message);
-      } finally {
-        setLoadingState(false);
-      }
-    }
-    fetchImages();
-  }, []);
-
+  const [imageData, loadingState, errorDuringFetch] = useFetch("/api/images");
   return (
     <>
       <h2>All Images</h2>
--- frontend/src/images/ImageDetails.jsx
+++ frontend/src/images/ImageDetails.jsx
@@ -1,21 +1,17 @@
-import { useState } from "react";
-import { MainLayout } from "../MainLayout.jsx";
-import { fetchById } from "./ImageFetcher.js";
 import { useParams } from "react-router";
+import { useFetch } from "./useFetch.js";
 
 export function ImageDetails() {
   const { imageId } = useParams();
-  const [image, _setImage] = useState(fetchById(imageId));
-  if (!image) {
-    return (
-      <>
-        <h2>Image not found</h2>
-      </>
-    );
-  }
-
+  const [imageData, loadingState, errorDuringFetch] = useFetch("/api/images");
+  const image = imageData[imageId];
+  if (loadingState) return <span>Loading...</span>;
+  if (errorDuringFetch.length !== 0) return <span>{errorDuringFetch}</span>;
+  if (!image) return <span>Image not found</span>;
   return (
     <>
+      {loadingState && <span>Loading...</span>}
+      {errorDuringFetch.length !== 0 && <span>{errorDuringFetch}</span>}
       <h2>{image.name}</h2>
       <p>By {image.author.username}</p>
       <img className="ImageDetails-img" src={image.src} alt={image.name} />
